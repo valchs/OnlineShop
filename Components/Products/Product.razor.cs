8b1beb0749cacf098432b64e38fc0dd3ccbc5dd6
valchs <valters.sudmalis@gmail.com>
2021-09-06 22:44:48 +0300

    home page and product list

---
+++ Components/Products/Product.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using OnlineShopLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Components.Products
+{
+    public partial class Product : ComponentBase
+    {
+        [Parameter] public ProductModel ProductModel { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+    }
+}
---
+++ Pages/Products.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using OnlineShopLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Pages
+{
+    public partial class Products : ComponentBase
+    {
+        public List<ProductModel> ProductList { get; set; } = new();
+
+        protected override void OnInitialized()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                var product = new ProductModel
+                {
+                    Title = $"Product {i}",
+                    Description = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+                    ImagePath = $"Assets/paint{i}.jpg"
+                };
+                ProductList.Add(product);
+            }
+        }
+    }
+}
